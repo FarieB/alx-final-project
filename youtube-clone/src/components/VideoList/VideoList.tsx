31bf6ff259f3e25c585757790fed9091e8b6ab53
FarieB <faraibande@gmail.com>
2025-01-19 07:03:31 -0800

    Updating  VideoList.tsx component to fetch the list of videos from the mock API and displays them dynamically

--- youtube-clone/src/components/VideoList/VideoList.tsx
+++ youtube-clone/src/components/VideoList/VideoList.tsx
@@ -1,19 +1,45 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import './VideoList.css';
 
-const VideoList = () => {
+interface Video {
+  id: number;
+  title: string;
+  description: string;
+  thumbnail: string;
+  userImage: string;
+}
+
+const VideoList: React.FC = () => {
+  const [videos, setVideos] = useState<Video[]>([]);
+
+  useEffect(() => {
+    const fetchVideos = async () => {
+      try {
+        const response = await fetch('http://localhost:5000/videos');
+        const data = await response.json();
+        setVideos(data);
+      } catch (error) {
+        console.error('Error fetching videos:', error);
+      }
+    };
+
+    fetchVideos();
+  }, []);
+
   return (
     <div className="list-container">
-      <div className="vid-list">
-        <img className="thumbnail" src="/images/video-thumbnail.jpg" alt="Video Thumbnail" />
-        <div className="flex-div">
-          <img src="/images/user1.jpg" alt="User" />
-          <div className="vid-info">
-            <a href="#">Video Title</a>
-            <p>Video description...</p>
+      {videos.map((video) => (
+        <div className="vid-list" key={video.id}>
+          <img className="thumbnail" src={video.thumbnail} alt={video.title} />
+          <div className="flex-div">
+            <img src={video.userImage} alt="User" />
+            <div className="vid-info">
+              <a href="#">{video.title}</a>
+              <p>{video.description}</p>
+            </div>
           </div>
         </div>
-      </div>
+      ))}
     </div>
   );
 };
