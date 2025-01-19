658b4f1ce7e637006a0dc105d08fd0407a322708
FarieB <faraibande@gmail.com>
2025-01-19 11:33:26 -0800

    Restore files from commit b54a8ce

---
+++ youtube-clone/src/App.tsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import Navbar from './components/Navbar/Navbar';
+import Sidebar from './components/Sidebar/Sidebar';
+import VideoList from './components/VideoList/VideoList';
+import VideoPlayer from './components/VideoPlayer/VideoPlayer';
+
+const App = () => {
+  return (
+    <div className="app">
+      <Navbar />
+      <Sidebar />
+      <div className="content">
+        <VideoList />
+        <VideoPlayer />
+      </div>
+    </div>
+  );
+};
+
+export default App;
+
