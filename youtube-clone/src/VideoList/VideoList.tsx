2643fb6d3ad10e94f0ecb4f255ee7e9a8a24f095
FarieB <faraibande@gmail.com>
2025-01-20 02:35:24 -0800

    Creating the db.json mock database

--- youtube-clone/src/VideoList/VideoList.tsx
+++ youtube-clone/src/VideoList/VideoList.tsx
@@ -1,19 +1,30 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import './VideoList.css';
 
 const VideoList = () => {
+  const [videos, setVideos] = useState([]);
+
+  useEffect(() => {
+    fetch('http://localhost:5000/videos')
+      .then(response => response.json())
+      .then(data => setVideos(data))
+      .catch(error => console.error('Error fetching videos:', error));
+  }, []);
+
   return (
     <div className="list-container">
-      <div className="vid-list">
-        <img className="thumbnail" src="/images/video-thumbnail.jpg" alt="Video Thumbnail" />
-        <div className="flex-div">
-          <img src="/images/user1.jpg" alt="User" />
-          <div className="vid-info">
-            <a href="#">Video Title</a>
-            <p>Video description...</p>
+      {videos.map(video => (
+        <div key={video.id} className="vid-list">
+          <img className="thumbnail" src={video.thumbnail} alt={video.title} />
+          <div className="flex-div">
+            <img src={video.userImage} alt={video.userName} />
+            <div className="vid-info">
+              <a href="#">{video.title}</a>
+              <p>{video.description}</p>
+            </div>
           </div>
         </div>
-      </div>
+      ))}
     </div>
   );
 };
