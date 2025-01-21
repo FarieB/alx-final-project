4c6eee6030d1daeb2f398e85b262e4962c70fd2b
FarieB <faraibande@gmail.com>
2025-01-21 16:09:10 +0200

    Fixing feed

--- youtube-clone/src/components/index.js
+++ youtube-clone/src/components/index.js
@@ -5,4 +5,3 @@ export { default as VideoDetail } from './VideoDetail';
 export { default as SearchFeed } from './SearchFeed';
 export { default as SearchBar } from './SearchBar';
 export { default as Sidebar } from './Sidebar';
-export { default as Feed } from './Feed';
